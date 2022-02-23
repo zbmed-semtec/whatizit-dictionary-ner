93206bfbede7eadb98ec6a9fc0e91ce08ea43683
rohitharavinder <rohitha0112@gmail.com>
2022-02-23 16:38:05 +0100

    Tests initial commit

--- code/parse_csv.py
+++ code/parse_csv.py
@@ -12,65 +12,69 @@ if not os.path.exists("../data/logging"):
 logging.basicConfig(filename='../data/logging/obsolete.log', filemode='w', level=logging.DEBUG)
 
 
-def get_metadata(input_file: str) -> dict:
-    """Parses the csv file and returns a dictionary of Class IDs with the corresponding metadata.
-    Obsolete terms are logged into a log file.
-    Input : csv file
-    Output : metadata -> Dictionary of metadata
-             Class ID : {"Term" : Preferred Label,
-                        "Synonyms" : Synonym(s),
-                        "CUI: Concept identifier(s),
-                        "Semantic Types" : Semantic Type UMLS property
-                        }
-    """
-    data = pd.read_csv(input_file, low_memory=False)
-    if True in data['Obsolete'].unique():
-        pos = data.index[data['Obsolete'] == True]
-        for ind in pos:
-            logging.info((", ".join(str(x) for x in data.iloc[ind])).replace("nan", ''))
-        data.drop(data.index[pos], inplace=True)
-    metadata = dict()
-    for i in range(len(data)):
-        metadata[data['Class ID'].iloc[i]] = {'Term': data['Preferred Label'].iloc[i]}
-        if pd.isnull(data['Synonyms'].iloc[i]) is False:
-            metadata[data['Class ID'].iloc[i]]['Synonyms'] = data['Synonyms'].iloc[i].split("|")
-        if pd.isnull(data['CUI'].iloc[i]) is False:
-            metadata[data['Class ID'].iloc[i]]['CUI'] = data['CUI'].iloc[i].replace("|", ", ")
-        if pd.isnull(data['Semantic Types'].iloc[i]) is False:
-            metadata[data['Class ID'].iloc[i]]['Semantic Types'] = data['Semantic Types'].iloc[i].replace("|", ", ")
-    return metadata
+class CSVParser:
+    def __init__(self, input_file: str, output_file: str, vocab: str):
+        self.metadata = self.get_metadata(input_file)
+        self.write_mwt(output_file, vocab)
 
+    def get_metadata(self, input_file) -> dict:
+        """Parses the csv file and returns a dictionary of Class IDs with the corresponding metadata.
+        Obsolete terms are logged into a log file.
+        Input : csv file
+        Output : metadata -> Dictionary of metadata
+                 Class ID : {"Term" : Preferred Label,
+                            "Synonyms" : Synonym(s),
+                            "CUI: Concept identifier(s),
+                            "Semantic Types" : Semantic Type UMLS property
+                            }
+        """
+        data = pd.read_csv(input_file, engine='python')
+        if True in data['Obsolete'].unique():
+            pos = data.index[data['Obsolete'] == True]
+            for ind in pos:
+                logging.info((", ".join(str(x) for x in data.iloc[ind])).replace("nan", ''))
+            data.drop(data.index[pos], inplace=True)
+        metadata = dict()
+        for i in range(len(data)):
+            metadata[data['Class ID'].iloc[i]] = {'Term': data['Preferred Label'].iloc[i]}
+            if pd.isnull(data['Synonyms'].iloc[i]) is False:
+                metadata[data['Class ID'].iloc[i]]['Synonyms'] = data['Synonyms'].iloc[i].split("|")
+            if pd.isnull(data['CUI'].iloc[i]) is False:
+                metadata[data['Class ID'].iloc[i]]['CUI'] = data['CUI'].iloc[i].replace("|", ", ")
+            if pd.isnull(data['Semantic Types'].iloc[i]) is False:
+                metadata[data['Class ID'].iloc[i]]['Semantic Types'] = data['Semantic Types'].iloc[i].replace("|", ", ")
+        return metadata
 
-def write_mwt(metadata_dict: dict, output_filename: str, vocab: str):
-    """Takes the data from the metadata dictionary and writes it to an output mwt file
-    Input: metadata_dict -> Dictionary of metadata
-           output_filename -> Output file path
-    Output : MWT file
-    """
-    with open(output_filename, 'w') as output:
-        output.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-        output.write('<mwt xmlns:z="https://github.com/zbmed-semtec/whatizit-dictionary-ner/">\n')
-        output.write("<template><z:{} id='%1' cui='%2' semantics='%3'>%0</z:{}></template>\n\n".format(vocab, vocab))
+    def write_mwt(self, output_filename: str, vocab: str):
+        """Takes the data from the metadata dictionary and writes it to an output mwt file
+        Input: metadata_dict -> Dictionary of metadata
+               output_filename -> Output file path
+        Output : MWT file
+        """
+        with open(output_filename, 'w') as output:
+            output.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+            output.write('<mwt xmlns:z="https://github.com/zbmed-semtec/whatizit-dictionary-ner/">\n')
+            output.write("<template><z:{} id='%1' cui='%2' semantics='%3'>%0</z:{}></template>\n\n".format(vocab, vocab))
 
-        for class_id, metadata in metadata_dict.items():
-            if "CUI" and "Semantic Types" in metadata:
-                output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], metadata['Term']))
-                if "Synonyms" in metadata:
+            for class_id, metadata in self.metadata.items():
+                if "CUI" and "Semantic Types" in metadata:
+                    output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], metadata['Term']))
+                    if "Synonyms" in metadata:
+                        n = 0
+                        while n < len(metadata['Synonyms']):
+                            synonym = metadata['Synonyms'][n]
+                            output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], synonym))
+                            n = n + 1
+                elif "Synonyms" in metadata:
                     n = 0
                     while n < len(metadata['Synonyms']):
                         synonym = metadata['Synonyms'][n]
-                        output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], synonym))
+                        output.write('<t p1="{}">{}</t>\n'.format(class_id, synonym))
                         n = n + 1
-            elif "Synonyms" in metadata:
-                n = 0
-                while n < len(metadata['Synonyms']):
-                    synonym = metadata['Synonyms'][n]
-                    output.write('<t p1="{}">{}</t>\n'.format(class_id, synonym))
-                    n = n + 1
-            else:
-                output.write('<t p1="{}">{}</t>\n'.format(class_id, metadata['Term']))
-        output.write("\n</mwt>")
-    return
+                else:
+                    output.write('<t p1="{}">{}</t>\n'.format(class_id, metadata['Term']))
+            output.write("\n</mwt>")
+        return
 
 
 if __name__ == "__main__":
@@ -79,5 +83,5 @@ if __name__ == "__main__":
     parser.add_argument("--output", type=str, help="Path for output mwt file")
     parser.add_argument("--vocab", type=str, help="Namespace for controlled vocabulary for mwt file")
     args = parser.parse_args()
-    meta = get_metadata(args.input)
-    write_mwt(meta, args.output, args.vocab)
+    CSVParser(args.input, args.output, args.vocab)
+
--- code/test_cases.py
+++ code/test_cases.py
@@ -1,10 +1,41 @@
 import unittest
-
-
+import re
+from parse_csv import CSVParser
+from parser import Parser
 
 class TestCases(unittest.TestCase):
-    
-    def func():
-        pass
 
+    def test_namespace(self):
+        """Tests for the namespace of the mwt dictionary file."""
+
+        CSVParser('../data/input/sample_MESH.csv', '../data/output/test1.mwt', 'MESH')
+        with open('../data/output/test1.mwt', 'r') as test_output:
+            header = test_output.read().splitlines()[:3]
+            assert header[0] == "<?xml version='1.0' encoding='UTF-8'?>"
+            assert header[1] == '<mwt xmlns:z="https://github.com/zbmed-semtec/whatizit-dictionary-ner/">'
+            assert header[2] == "<template><z:MESH id='%1' cui='%2' semantics='%3'>%0</z:MESH></template>"
+
+    def test_labels(self):
+        """Checks if all vocabulary terms are included in both output dictionaries, ttl and csv."""
+
+        CSVParser('../data/input/sample_MESH.csv', '../data/output/test1.mwt', 'MESH')
+        Parser("../code/Config.yaml")
+        with open('../data/output/test1.mwt', 'r') as test_output1, open('../data/output/test2.mwt', 'r') as test_output2:
+            lines_csv = test_output1.read().splitlines()[4:-1]
+            lines_ttl = test_output2.read().splitlines()[4:-1]
+        labels_csv = set()
+        labels_ttl = set()
+        for line in lines_csv:
+            label = re.search('(?<=>)(.*?)(?=</)', line)
+            labels_csv.add(label.group())
+        for line in lines_ttl:
+            label = re.search('(?<=>)(.*?)(?=</)', line)
+            labels_ttl.add(label.group())
+        assert set(labels_csv) == set(labels_ttl)
+
+    def test_count(self):
+        # Add test for count
+
+if __name__ == '__main__':
+    unittest.main()
 
