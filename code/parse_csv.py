ad2d7e5017afbc18a98ac3c76fb4611ec5309e38
Rohitha Ravinder <rohitharavinder@Rohithas-MacBook-Air.local>
2022-06-06 22:26:26 +0530

    Updated documentation to numpydoc

--- code/parse_csv.py
+++ code/parse_csv.py
@@ -16,6 +16,16 @@ logging.basicConfig(filename='../data/logging/obsolete.log', filemode='w', level
 
 
 class CSVParser:
+    """
+    Class to parse the MESH CSV file, extract metadata for each MeSH vocabulary term and output a mwt dictionary.
+    The dictionary consists of the MeSH ID, Preferred Label, Synonyms, CUI and Semantic Types for each term.
+    Attributes
+    ----------
+    vocab : str
+        Name of the vocabulary.
+    metadata : dict
+        Dictionary containing the corresponding metadata for each term.
+    """
     def __init__(self, input_file: str, output_file: str, vocab: str):
         self.input_file = input_file
         self.output_file = output_file
@@ -25,13 +35,15 @@ class CSVParser:
     def get_metadata(self) -> dict:
         """Parses the csv file and returns a dictionary of Class IDs with the corresponding metadata.
         Obsolete terms are logged into a log file.
-        Returns :
-                metadata : Dictionary of metadata
-                Class ID : {"Term" : Preferred Label,
-                            "Synonyms" : Synonym(s),
-                            "CUI: Concept identifier(s),
-                            "Semantic Types" : Semantic Type UMLS property
-                            }
+        Returns
+        ----------
+        metadata : dict
+            Dictionary of metadata
+            Class ID : {"Term" : Preferred Label,
+                        "Synonyms" : Synonym(s),
+                        "CUI: Concept identifier(s),
+                        "Semantic Types" : Semantic Type UMLS property
+                        }
         """
         data = pd.read_csv(self.input_file, engine='python')
         if True in data['Obsolete'].unique():
@@ -48,11 +60,18 @@ class CSVParser:
                 if pd.isnull(data['CUI'].iloc[i]) is False:
                     metadata[data['Class ID'].iloc[i]]['CUI'] = data['CUI'].iloc[i].replace("|", ", ")
                 if pd.isnull(data['Semantic Types'].iloc[i]) is False:
-                    metadata[data['Class ID'].iloc[i]]['Semantic Types'] = data['Semantic Types'].iloc[i].replace("|", ", ")
+                    metadata[data['Class ID'].iloc[i]]['Semantic Types'] = \
+                        data['Semantic Types'].iloc[i].replace("|", ", ")
         return metadata
 
-    def replace_chars(self, text):
-        """Replaces special characters that invalidate the mwt format with the correct syntax."""
+    @staticmethod
+    def replace_chars(text) -> str:
+        """Replaces special characters that invalidate the mwt format with the correct syntax.
+        Parameters
+        ----------
+        text : str
+            Term and the Synonyms.
+        """
         text = text.replace("&", "&amp;")
         text = text.replace('"', "&quot;")
         text = text.replace("'", "&apos;")
@@ -60,27 +79,30 @@ class CSVParser:
         text = text.replace(">", "&gt;")
         return text
 
-    def write_mwt(self):
+    def write_mwt(self) -> None:
         """Takes the data from the metadata dictionary and writes it to an output mwt file"""
         with open(self.output_file, 'w') as output:
             output.write("<?xml version='1.0' encoding='UTF-8'?>\n")
             output.write('<mwt xmlns:z="https://github.com/zbmed-semtec/whatizit-dictionary-ner#">\n')
             n_parameters = len(self.metadata[max(self.metadata, key=lambda v:len(self.metadata[v]))])
             if n_parameters > 2:
-                output.write("<template><z:{} id='%1' cui='%2' sty='%3'>%0</z:{}></template>\n\n".format(self.vocab, self.vocab))
+                output.write("<template><z:{} id='%1' cui='%2' sty='%3'>%0</z:{}></template>\n\n".
+                             format(self.vocab, self.vocab))
             else:
                 output.write("<template><z:{} id='%1'>%0</z:{}></template>\n\n".format(self.vocab, self.vocab))
 
             for class_id, metadata in self.metadata.items():
                 if "CUI" and "Semantic Types" in metadata:
                     metadata['Term'] = self.replace_chars(metadata['Term'])
-                    output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], metadata['Term']))
+                    output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.
+                                 format(class_id, metadata['CUI'], metadata['Semantic Types'], metadata['Term']))
                     if "Synonyms" in metadata:
                         n = 0
                         while n < len(metadata['Synonyms']):
                             synonym = metadata['Synonyms'][n]
                             synonym = self.replace_chars(synonym)
-                            output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(class_id, metadata['CUI'], metadata['Semantic Types'], synonym))
+                            output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.
+                                         format(class_id, metadata['CUI'], metadata['Semantic Types'], synonym))
                             n = n + 1
                 elif "Synonyms" in metadata:
                     metadata['Term'] = self.replace_chars(metadata['Term'])
@@ -106,4 +128,3 @@ if __name__ == "__main__":
     args = parser.parse_args()
     csv_parser = CSVParser(args.input, args.output, args.vocab)
     csv_parser.write_mwt()
-
--- code/parser.py
+++ code/parser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from rdflib import Graph, RDF
 from rdflib.namespace import SKOS, RDFS, OWL
 from rdflib import Namespace
@@ -5,7 +6,15 @@ import yaml
 
 
 class Parameters:
-    
+    """
+    Class for input parameters to parse the MESH TTL file.
+    Attributes
+    ----------
+    vocab_name : str
+        Name of vocabulary.
+    namespace : str
+        Namespace for the mwt dictionary.
+    """
     def __init__(self, config_file):
         
         with open(config_file) as file:
@@ -22,34 +31,42 @@ class Parameters:
 
 
 class Parser(Parameters):
-    
+    """
+    Class to parse the MESH TTL file, extract metadata for each MeSH vocabulary term and output a mwt dictionary.
+    The dictionary consists of the MeSH ID, Preferred Label, Synonyms, CUI and Semantic Types for each term.
+    Attributes
+    ----------
+
+    """
     def __init__(self, config_file):
         super().__init__(config_file)
         self.g = Graph()
         self.g.parse(self.input_file, format=self.file_type)
         self.__dictionary = self.create_dictionary()
         
-    def get_dictionary(self):
+    def get_dictionary(self) -> dict:
         """Returns the dictionary with IDs as keys and labels, synonyms as values.
 
-        Returns:
-            dict (dict): Dictionary with IDs as keys and labels, synonyms as values.
+        Returns
+        ----------
+        dict : dict
+            Dictionary with IDs as keys and labels, synonyms as values.
         """
         return self.__dictionary
 
-    def create_dictionary(self):
-        """  file into a dictionary with IDs as keys and
+    def create_dictionary(self) -> dict:
+        """Converts file into a dictionary with IDs as keys and
         labels, synonyms as values.
 
-        Returns:
-            dic (dict): Dictionary with IDs as keys and labels, synonyms as values.
-
+        Returns
+        ----------
+        dic : dict
+            Dictionary with IDs as keys and labels, synonyms as values.
         """
         dic = {}
         umls = Namespace("http://bioportal.bioontology.org/ontologies/umls/")
         for owlClass in self.g.subjects(RDF.type, OWL.Class):
             if owlClass.startswith("http://purl.bioontology.org/ontology/MESH/"):
-                #print(owlClass)
                 for notation in self.g.objects(owlClass, SKOS.notation):
                     dic[str(owlClass)] = {}
                 for label in self.g.objects(owlClass, SKOS.prefLabel):
@@ -65,8 +82,14 @@ class Parser(Parameters):
                     dic[str(owlClass)]["cui"].append(str(cui))
         return dic
 
-    def replace_chars(self, text):
-        """Replaces special characters that invalidate the mwt format with the correct syntax."""
+    @staticmethod
+    def replace_chars(text) -> str:
+        """Replaces special characters that invalidate the mwt format with the correct syntax.
+        Parameters
+        ----------
+        text : str
+            Term and the Synonyms.
+        """
         text = text.replace("&", "&amp;")
         text = text.replace('"', "&quot;")
         text = text.replace("'", "&apos;")
@@ -74,29 +97,28 @@ class Parser(Parameters):
         text = text.replace(">", "&gt;")
         return text
 
-    def create_mwt_file(self):
-        """creates a MWT file from a dictionary with IDs as keys and labels, synonyms as values.
+    def create_mwt_file(self) -> None:
+        """Creates a MWT file from a dictionary with IDs as keys and labels, synonyms as values.
         saves the file to the output_file destination.
         """
-        
         with open(self.output_file, 'w') as output:
             output.write("<?xml version='1.0' encoding='UTF-8'?>\n")
             output.write('<mwt xmlns:z="{}">\n'.format(self.namespace))
             n_parameters = len(self.__dictionary[max(self.__dictionary, key=lambda v: len(self.__dictionary[v]))])
             if n_parameters > 2:
-                output.write("<template><z:{} id='%1' cui='%2' sty='%3'>%0</z:{}></template>\n\n".format(self.vocab_name, self.vocab_name))
+                output.write("<template><z:{} id='%1' cui='%2' sty='%3'>%0</z:{}></template>\n\n".
+                             format(self.vocab_name, self.vocab_name))
             else:
-                output.write("<template><z:{} id='%1'>%0</z:{}></template>\n\n".format(self.vocab_name, self.vocab_name))
+                output.write("<template><z:{} id='%1'>%0</z:{}></template>\n\n".
+                             format(self.vocab_name, self.vocab_name))
             
             for id, metadata in self.__dictionary.items():
                 if 'cui' and 'semantic_types' in metadata:
-                    #print("cui and st")
                     cui = (", ".join(map(str, metadata["cui"]))).replace("'", "")
-                    print(cui)
                     semantic_types = ", ".join(map(str, metadata["semantic_types"]))
-                    print(semantic_types)
                     metadata['label'] = self.replace_chars(metadata["label"])
-                    output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(id, cui, semantic_types, metadata['label']))
+                    output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.
+                                 format(id, cui, semantic_types, metadata['label']))
                     if 'synonyms' in metadata:
                         n = 0
                         while n < len(metadata['synonyms']):
@@ -105,7 +127,6 @@ class Parser(Parameters):
                             output.write('<t p1="{}" p2="{}" p3="{}">{}</t>\n'.format(id, cui, semantic_types, synonym))
                             n = n + 1
                 elif "synonyms" in metadata:
-                    print("only synonyms")
                     metadata['label'] = self.replace_chars(metadata['label'])
                     output.write('<t p1="{}">{}</t>\n'.format(id, metadata['label']))
                     n = 0
@@ -115,7 +136,6 @@ class Parser(Parameters):
                         output.write('<t p1="{}">{}</t>\n'.format(id, synonym))
                         n = n + 1
                 else:
-                    print("none")
                     metadata['label'] = self.replace_chars(metadata['label'])
                     output.write('<t p1="{}">{}</t>\n'.format(id, metadata['label']))
             output.write("\n</mwt>")
